da9392a7b0f904193ca4cbdf3ce29d201c1aaf16
DESKTOP-UVTH9E9\MainUser <josue.suazo@iteso.mx>
2026-04-20 19:26:06 -0600

    feat: add article crud backend tests

---
+++ backend/tests/setupTests.js
@@ -0,0 +1,20 @@
+/*
+ Setup for Postgres database connection during tests
+*/
+
+require('dotenv').config({ path: './backend/.env' });
+import { beforeAll, afterAll } from 'vitest';
+const db = require('../models');
+
+beforeAll(async () => {
+    // sets up database for tests
+    try {
+        // sync the database
+        await db.sequelize.sync({ force: true });
+        console.log('Test Database synced.');
+    } catch (err) {
+        // throw error if unable to connect to database
+        console.error('Unable to connect to the test database:', err);
+        process.exit(1);
+    }
+});
--- vitest.config.js
+++ vitest.config.js
@@ -1,12 +1,30 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
   test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "frontend/src/setupTests.js",
-    css: true,
+    projects: [
+      {
+        plugins: [react()],
+        test: {
+          name: 'frontend',
+          root: './frontend',
+          globals: true,
+          environment: "jsdom",
+          setupFiles: "src/setupTests.js",
+          css: true,
+        },
+      },
+      {
+        test: {
+          name: 'backend',
+          root: './backend',
+          globals: true,
+          environment: "node",
+          setupFiles: "tests/setupTests.js",
+        },
+      },
+    ],
   },
 });
